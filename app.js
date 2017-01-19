2d0b3683344e35a1f3dc4a651430464704e7f48c
Walker Willetts <wlkrwilletts@gmail.com>
2017-01-19 16:08:24 -0500

    Added conditional check to make sure there were results for search.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+  // Setup all required variables
   var configs = require('./config.js');
   var OAuth2 = require('oauth').OAuth2;
   var https = require('https');
@@ -7,20 +7,21 @@
   var bodyParser = require('body-parser');
   var searcher = require('./app.js');
 
+  // Create and configure new express app
   var app = express();
-
   app.use(bodyParser.urlencoded({ extended: true}));
-
   app.use(bodyParser.json());
 
+  // Routing for post at '/' URL
   app.post('/', function(req,res) {
+
+    // Variables for POST
     var twilio = require('twilio');
     var twiml = new twilio.TwimlResponse();
     var SEARCH_QUERY = req.body.Body.trim().replace(' ', '_');
     var oAuth2 = new OAuth2(configs.KEY,configs.SECRET, 'https://api.twitter.com/', null, 'oauth2/token', null);
 
-
-
+    // Get OAuth2 Access for Twitter API
     oAuth2.getOAuthAccessToken('', { 'grant_type' : 'client_credentials'}, function(e, access_token) {
       var options = {
         hostname: 'api.twitter.com',
@@ -31,7 +32,9 @@
         }
       };
 
+      // Get the tweets from Twitter API
       https.get(options, function(result) {
+
         var buffer = '';
         result.setEncoding('utf8');
 
@@ -40,6 +43,7 @@
         });
 
         result.on('end', function() {
+          // Build the message response from recieved tweets
           var statuses = JSON.parse(buffer).statuses;
           var tweets = '';
           for(var i = 0; i < statuses.length; i++){
@@ -48,15 +52,18 @@
             if((status.length + tweets.length) > 1600) break;
             tweets = tweets + status;
           }
-          console.log(tweets);
+
+          // Check to make sure there are tweets
+          if(statuses.length == 0) tweets = "Sorry, no tweets were found.";
+
+          // Create message and respond
           twiml.message(tweets);
           res.writeHead(200, {'Content-Type' : 'text/xml'});
           res.end(twiml.toString());
         });
       });
-
     });
-
   });
 
+  // Initialize server to listen for texts
   http.createServer(app).listen((process.env.PORT || 1337), function() {});
